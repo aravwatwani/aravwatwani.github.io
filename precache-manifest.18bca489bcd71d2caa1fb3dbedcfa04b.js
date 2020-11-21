5b17a40d04e36cad571fb54e36b6c5adbd068a47
arav <aravwatwani@gmail.com>
2020-11-21 03:38:03 -0800

    Updates

---
+++ precache-manifest.18bca489bcd71d2caa1fb3dbedcfa04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfd3cc9f9778d3e650352cd59d64da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1ddd3f287aebb828ae",
+    "url": "/static/css/main.46c492b4.chunk.css"
+  },
+  {
+    "revision": "3780d47cea2ab6d19738",
+    "url": "/static/js/2.a126be39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a126be39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1ddd3f287aebb828ae",
+    "url": "/static/js/main.f53e4d92.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  }
+]);
